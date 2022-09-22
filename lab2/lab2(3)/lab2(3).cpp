6c03111de323c714fce88ccfc5c7023179e1f16b
gerushenka <112763670+gerushenka@users.noreply.github.com>
2022-09-22 15:50:38 +0300

    Add files via upload

---
+++ lab2/lab2(3)/lab2(3).cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#define scanf scanf_s
+int main()
+{
+	int kolc = 0, kola = 0, kolb = 0, a = 0, A = 0, b = 0, B = 0, C = 0;
+	printf("Enter a sides of the rectangle \nA = ");;
+	scanf("%d", &A);
+	printf("B = ");
+	scanf("%d", &B);
+	printf("Enter a side of the square\nC = ");
+	scanf("%d", &C);
+	a = A;
+	while (a >= C) {
+		a -= C;
+		kola++;
+	}
+	b = B; 
+	while (b >= C) {
+		b -= C;
+		kolb++;
+
+	}
+	for (int i = 1; i <= kolb; i++) {
+		kolc += kola;
+	}
+	printf("There are %d squares can be placed in the rectangle\n", kolc);
+
+
+
+}
---
+++ lab2/lab2/lab2(1).cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#define scanf scanf_s
+int main()
+{
+    double sum = 0, a = 0, i = 0, n = 0;
+    printf("Enter an original number ");
+    scanf("%lf", &a);
+    while (sum < a) {
+        i++;
+        sum += 1 / i;
+    }
+    printf("The first number that bigger than a is %.6lf\n", sum);
+
+}
+
