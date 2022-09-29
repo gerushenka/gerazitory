339de5ba5b636b323112ed4e54366b05b1e6ccb5
gerushenka <112763670+gerushenka@users.noreply.github.com>
2022-09-29 18:00:27 +0300

    Add files via upload

---
+++ lab1(2)/lab1(2)/lab1(2).cpp
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    double k = 0, a = 0, b = 0, c = 0;
+    printf("Enter the first side ");
+    scanf_s("%lf", &a);
+    printf("Enter the second side ");
+    scanf_s("%lf", &b);
+    printf("Enter the third side ");
+    scanf_s("%lf", &c);
+    if (a == b) {
+        printf("\nThe triangle is isosceles");
+        return 0;
+    }
+    if (a == c) {
+        printf("\nThe triangle is isosceles");
+        return 0;
+    }
+    if (c == b) {
+        printf("\nThe triangle is isosceles");
+        return 0;
+    }
+    printf("\nthe triangle isn't isosceles");
+    return 0;
+
+
+}
+
---
+++ lab1(4)/lab1(4)/lab1(4).cpp
@@ -0,0 +1,48 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int number = 0;
+    double decim = -1, m = -1, kilom = -1, santim = -1, milim = -1;
+    printf("choose the unit of measure\n1.decimeter\n2.santimeter\n3.meter\n4.kilometer\n5.milimeter\n");
+    scanf_s("%d", &number);
+    switch (number) {
+    case 1:
+        printf("Enter the number of decimeters ");
+        scanf_s("%d", &decim);
+        m = decim / 10;
+        printf("\n%.2lf decimeters is %.2lf meters", decim, m);
+        break;
+    case 2:
+        printf("Enter the number of santimeters ");
+        scanf_s("%lf", &santim);
+        m = santim / 100;
+        printf("\n%.2lf santimeters is %.2lf meters", santim, m);
+        break;
+    case 3:
+        printf("Enter the number of meters ");
+        scanf_s("%lf", &m);
+        printf("\n%.2lf meters is %.2lf meters", m, m);
+        break;
+    case 4:
+        printf("Enter the number of kilometers ");
+        scanf_s("%lf", &kilom);
+        m = kilom * 1000;
+        printf("\n%.2lf kilometers is %.2lf meters", kilom, m);
+        break;
+    case 5:
+        printf("Enter the number of milimeters ");
+        scanf_s("%lf", &milim);
+        m = milim / 1000;
+        printf("\n%.2lf milimeters is %.2lf meters", milim, m);
+        break;
+    }
+
+
+
+
+
+    return 0;
+
+}
+
