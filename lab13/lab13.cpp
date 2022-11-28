021a5742aa0222b6f82abea6d5acff3a03a41da2
gerushenka <112763670+gerushenka@users.noreply.github.com>
2022-11-28 23:11:33 +0300

    12345678

---
+++ lab13/lab13.cpp
@@ -0,0 +1,27 @@
+int main()
+{
+	const double pi = 3.1415, koren2 = 1.4142;
+	double  k = 0, S1 = 0, S2 = 0;
+	double 	a = 0, r = 0, d = 0;
+	printf("Enter area of circle ");
+	scanf_s("%lf", &S1);
+	printf("Enter square area ");
+	scanf_s("%lf", &S2);
+	r = sqrt(S1 / pi);
+	a = sqrt(S2);
+	d = a * koren2;
+	if (r * 2 == a) {
+		printf("\nA circle can be inscribed in a square");
+		k++;
+	}
+	if (r * 2 == d) {
+		printf("\nA square can be inscribed in a circle");
+		k++;
+	}
+	if (k == 0) {
+		printf("\nA square can't be inscribed in a circle and a circle can't be inscriibed in a square");
+
+	}
+	return 0;
+
+}
