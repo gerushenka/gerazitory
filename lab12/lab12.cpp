5ef2bee541bbc5f585aedfa9fdc2585dbbef3488
german <112763670+gerushenka@users.noreply.github.com>
2022-12-08 00:28:05 +0300

    Update lab12.cpp

--- lab12/lab12.cpp
+++ lab12/lab12.cpp
@@ -1,8 +1,10 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 
 int main()
 {
-    double k = 0, a = 0, b = 0, c = 0;
+    double a = 0;
+    double b = 0; 
+    double c = 0;
     printf("Enter the first side ");
     scanf_s("%lf", &a);
     printf("Enter the second side ");
