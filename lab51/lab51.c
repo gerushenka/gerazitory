078bf85ad1547a18818164ff0a172b7c7e6120d9
gerushenka <112763670+gerushenka@users.noreply.github.com>
2022-12-08 16:40:06 +0300

    123

--- lab51/lab51.c
+++ lab51/lab51.c
@@ -7,29 +7,29 @@
 
 
 int checkinput(int a, int m, int z) {
-    while (scanf("%d", &a) != 1 || getchar() != '\n' || a < m || a > z) {
+    while (scanf_s("%d", &a) != 1 || getchar() != '\n' || a < m || a > z) {
         printf("Pls enter a correct number!\n");
         rewind(stdin);
     }
     return a;
 }
-s;
+
 int main() {
     int n = 0, m = 0;;
     printf("Enter number of elements in the 1th array ");
     n = checkinput(n, 0, INT_MAX);
     printf("Enter number of elements in the 2th array ");
     m = checkinput(m, 0, INT_MAX);
-    float* mas = NULL;
+    int* mas = NULL;
     int* mas2 = NULL;
   /*  printf("\nIF you want fill array random elements input 1, IF no 0\n ");
         int k = 0;
         k = checkinput(k, 0, 1);
         if (k == 0) {*/
             printf("Enter the 1th array elements\n");
-            mas = (float*)malloc(n * sizeof(float));
-            //input_arr(mas, n);
-            scanf("%f", &mas[0]);
+            mas = (int*)malloc(n * sizeof(int));
+            input_arr(mas, n);
+            //scanf_s("%f", &mas[0]);
             printf("\n\nEnter the 2th array elements\n");
             mas2 = (int*)malloc(m * sizeof(int));
             input_arr(mas2, m);
